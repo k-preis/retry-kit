4998f2fdce9349b112df96d3c871fecf49c1c880
kpreis <preiskamil@gmail.com>
2025-10-05 20:33:55 +0200

    Build structure

---
+++ retry-kit-core/src/main/java/dev/kpreis/retrykit/core/App.java
@@ -0,0 +1,13 @@
+package dev.kpreis.retrykit.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ retry-kit-examples/src/main/java/dev/kpreis/retrykit/examples/App.java
@@ -0,0 +1,13 @@
+package dev.kpreis.retrykit.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
